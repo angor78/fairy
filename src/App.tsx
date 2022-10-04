662266e159aafd49bfa9ec9d4aa21a1eebaa46cd
Angor78 <angor78@gmail.com>
2022-10-04 20:14:20 +0300

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
+import s from './App.module.css';
 import './App.css';
+import dungeon from './assets/img/dungeon.jpg';
+import classNames from "classnames";
+import SmoothScroll from "./SmoothScroll";
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <SmoothScroll>
+      <header className={s.mainHeader}>
+        <div className={s.layers}>
+          <div className={s.layersHeader}>
+            <div className={s.layersCaption}>Шаблон с паралаксом</div>
+            <div className={s.layersTitle}>Чудный лес</div>
+          </div>
+          <div className={classNames(s.layer, s.layersBase)}></div>
+          <div className={classNames(s.layer, s.layersMiddle)}></div>
+          <div className={classNames(s.layer, s.layersFront)}></div>
+        </div>
       </header>
-    </div>
-  );
+      <article className={s.mainArticle} style={{backgroundImage: "url(" + dungeon + ")"}}>
+        <div className={s.mainArticleContent}>
+          <h2 className={s.mainArticleHeader}>Для своих проектов</h2>
+          <p className={s.paragraph}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto quo, vel! Eum, fuga
+            hic mollitia odio
+            officiis possimus quam! Aliquid beatae cum deleniti enim esse fugiat incidunt inventore necessitatibus
+            reprehenderit voluptates. Ducimus.
+          </p>
+        </div>
+        <div className={s.copy}>© Angor78</div>
+
+      </article>
+
+    </SmoothScroll>
+  )
 }
 
 export default App;
